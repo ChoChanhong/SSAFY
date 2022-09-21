293a188df64674109c7b7d12f660f5987fc110d9
김가흔 <rkgms0712@naver.com>
2022-09-21 14:32:17 +0900

    FE/CliSignup

--- FrontEnd/src/Components/SignTab1.jsx
+++ FrontEnd/src/Components/SignTab1.jsx
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 // import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export default function SignTab1(props) {
   const [check, setCheck] = useState(false);
@@ -18,20 +19,25 @@ export default function SignTab1(props) {
     <div>
       <div class="mt-5">
         <div>
-          <label >아이디</label>
-          <input placeholder="아이디를 입력해주세요"/>
+          <label>아이디</label>
+          <input placeholder="아이디를 입력해주세요" />
         </div>
         <div>
           <label>비밀번호</label>
-          <input placeholder="비밀번호를 입력해주세요"/>
-          <input placeholder="비밀번호를 재입력해주세요"/>
+          <input placeholder="비밀번호를 입력해주세요" />
+          <input placeholder="비밀번호를 재입력해주세요" />
         </div>
       </div>
       <span class="mt-2">약관</span>
+      <hr />
       <div>여긴 약관이 들어갈 자리입니당</div>
-      <input onChange={changeCheck} type="checkbox" />
-      <div>
-        <button onClick={Next}>다음 단계로</button>
+      <hr />
+      <div class="mt-3 d-flex">
+        <p>약관에 동의한다면 체크해주세요</p>
+        <input onChange={changeCheck} type="checkbox" />
+      </div>
+      <div class="mt-5 d-flex justify-content-center">
+        <Button onClick={Next} size="lg">다음 단계로</Button>
       </div>
     </div>
   );
