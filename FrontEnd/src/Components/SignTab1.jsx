53d0618c38c0280b373980c6d0081de82910406e
김가흔 <rkgms0712@naver.com>
2022-09-21 13:53:12 +0900

    FE/CliSignup

--- FrontEnd/src/Components/SignTab1.jsx
+++ FrontEnd/src/Components/SignTab1.jsx
@@ -1,38 +1,37 @@
-import {React,useState} from "react"
-import {Link} from "react-router-dom"
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function SignTab1(props){  
-    const [check,setCheck] = useState(false)
-    function Next(){
-        if(check){props.setStep(2)}
-        else{ alert("약관에 동의해 주세요") }
-        
+export default function SignTab1(props) {
+  const [check, setCheck] = useState(false);
+  function Next() {
+    if (check) {
+      props.setStep(2);
+    } else {
+      alert("약관에 동의해 주세요");
     }
-    function changeCheck(e){
-        console.log(e.target.checked)
-        setCheck(e.target.checked)
-        
-    }
-    return(
-     <div>
+  }
+  function changeCheck(e) {
+    console.log(e.target.checked);
+    setCheck(e.target.checked);
+  }
+  return (
+    <div>
+      <div>
         <div>
-            <div>
-                <label>아이디</label>
-                <input/>
-            </div>
-            <div>
-                <label>비밀번호</label>
-                <input/>
-                <input/>
-            </div>  
+          <label>아이디</label>
+          <input />
         </div>
         <div>
-            약관
+          <label>비밀번호</label>
+          <input />
+          <input />
         </div>
-        <input onChange={changeCheck} type="checkbox"/>
-        <div>
-            <button onClick={Next}>다음 단계로</button>
-        </div>
-     </div>   
-    )
+      </div>
+      <div>약관</div>
+      <input onChange={changeCheck} type="checkbox" />
+      <div>
+        <button onClick={Next}>다음 단계로</button>
+      </div>
+    </div>
+  );
 }
--- FrontEnd/src/pages/Cli/Signup.jsx
+++ FrontEnd/src/pages/Cli/Signup.jsx
@@ -1,36 +1,41 @@
-import {React,useState} from "react"
-import { useMediaQuery } from "react-responsive"
-import BackGround from "../../Components/BackGround"
-import SignTab1 from "../../Components/SignTab1"
-import SignTab2 from "../../Components/SignTab2"
+import { React, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import BackGround from "../../Components/BackGround";
+import SignTab1 from "../../Components/SignTab1";
+import SignTab2 from "../../Components/SignTab2";
+import "./Signup.css";
 
+export default function Signup() {
+  const [step, setStep] = useState(1);
+  const tab = {
+    1: <SignTab1 setStep={setStep} />,
+    2: <SignTab2 setStep={setStep} />,
+  };
 
-export default function Signup(){
-    const [step,setStep] = useState(1)
-    const tab = { 1:<SignTab1 setStep={setStep}/> , 2:<SignTab2 setStep={setStep}/>}
+  const isPc = useMediaQuery({
+    query: "(min-width:768px)",
+  });
 
-    const isPc = useMediaQuery ({
-        query: "(min-width:768px)"
-      });
-      
-    return(
-        <div id="webapp-containor">
-            <div>
-                { isPc ? <BackGround/> : null }
-            </div>
-            <div id="login">
-                <div>로고 회원가입</div>
-                <div>
-                    <div>
-                        <span>Step01 회원가입</span>
-                        <div>___________</div>
-                    </div>
-                    <div>
-                        <span>Step02 가입완료</span>
-                        <div>___________</div>
-                    </div>
-                </div>
-                {tab[step]}
-            </div>
+  return (
+    <div id="webapp-containor">
+      <div>{isPc ? <BackGround /> : null}</div>
+      <div id="login">
+        <div class="mt-3 d-flex justify-content-center">
+          <img src="img/002.png" alt="로고" width={50} />
+          <h2 class="fw-bold mt-3">회원가입</h2>
         </div>
-        )}
+        <div class="mt-5 d-flex">
+          <div>
+            <span>Step01 회원가입</span>
+            <div>___________</div>
+          </div>
+          <div>
+            <span>Step02 가입완료</span>
+            <div>___________</div>
+          </div>
+        </div>
+        {tab[step]}
+      </div>
+    </div>
+  );
+}
