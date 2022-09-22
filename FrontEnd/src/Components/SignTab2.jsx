66c4563684dcf3a5ffec52431e416936c0afb53c
김가흔 <rkgms0712@naver.com>
2022-09-22 11:53:55 +0900

    FE/CliSignup

--- FrontEnd/src/Components/SignTab2.jsx
+++ FrontEnd/src/Components/SignTab2.jsx
@@ -2,25 +2,32 @@ import { React } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignTab2(props) {
+  const h3 = {
+    color: "#5681EF",
+    marginTop: 50,
+    fontWeight: "bold",
+  };
   return (
-    <div class="mt-5 text-center">
-      <h1>가입이 완료 되었습니다</h1>
-      <h3>약쏙을 시작해보세요</h3>
-      <div style={{ margin: 150 }}>
+    <div class="text-center" style={{ marginTop: 180 }}>
+      <h1 style={{ fontWeight: "bold" }}>가입이 완료 되었습니다</h1>
+      <h3 style={h3}>약쏙을 시작해보세요</h3>
+      <div style={{ marginTop: 150 }}>
         <Link to="/login">
           <button
             style={{
               color: "white",
               backgroundColor: "#5FD068",
               height: 50,
-              width: 200,
+              width: 250,
               borderRadius: 10,
-              borderColor: "transparent"
+              borderColor: "transparent",
             }}
           >
             로그인
           </button>
         </Link>
+      </div>
+      <div>
         <Link to="">
           <button
             class="mt-3"
@@ -28,9 +35,9 @@ export default function SignTab2(props) {
               color: "white",
               backgroundColor: "#5681EF",
               height: 50,
-              width: 200,
+              width: 250,
               borderRadius: 10,
-              borderColor: "transparent"
+              borderColor: "transparent",
             }}
           >
             이용가이드
