50f2982846575dce4c901b4793ebec2bfc138280
김가흔 <rkgms0712@naver.com>
2022-09-27 22:31:36 +0900

    FE/Doc

--- FrontEnd/src/Components/DocNavbar.jsx
+++ FrontEnd/src/Components/DocNavbar.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import './DocNavbar.css'
+import { Link } from "react-router-dom";
+import "./DocNavbar.css";
+import logo from "./img/001.png";
 
-export default function DocNavbar(){
-
-    return(
-        <div id="nav">
-            <img id="logo"/>
-            <div>
-                <Link to ="/doc/generate">처방전 생성</Link>
-                <Link to ="/doc/log">처방전 조회</Link>
-                <Link to ="/doc/">마이페이지</Link>
-            </div>
-        </div>
-    )
+export default function DocNavbar() {
+  return (
+    <div id="nav">
+      <div>
+        {/* <img style={{ height: 100 }} src={logo} alt="로고" />
+        <img src={logo} alt="" /> */}
+        <img src={process.env.PUBLIC_URL + '/img/001.png'} alt="" />
+      </div>
+      <div>
+        <Link to="/doc/generate">처방전 생성</Link>
+        <Link to="/doc/log">처방전 조회</Link>
+        <Link to="/doc/">마이페이지</Link>
+      </div>
+    </div>
+  );
 }
--- FrontEnd/src/Components/LoginForm.jsx
+++ FrontEnd/src/Components/LoginForm.jsx
@@ -1,59 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
-import { injected } from '../lib/Connectors';
-import { useWeb3React } from '@web3-react/core';
-import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { injected } from "../lib/Connectors";
+import { useWeb3React } from "@web3-react/core";
+import { useNavigate } from "react-router-dom";
 
-import './LoginForm.css'
+import "./LoginForm.css";
 
-export default function LoginForm(){
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const { account, active, error, activate, deactivate } = useWeb3React();
 
-    const navigate = useNavigate();
-    const {
-        account,
-        active,
-        error,
-        activate,
-        deactivate,
-      } = useWeb3React();
+  const [id, setId] = useState("");
+  const [password, setPass] = useState("");
 
-    const [id,setId] = useState('')
-    const [password,setPass] = useState('')
+  function IdChange(e) {
+    setId(e.target.value);
+  }
+  function PasswordChange(e) {
+    setPass(e.target.value);
+  }
 
-    function IdChange(e){
-        setId(e.target.value)
-    }
-    function PasswordChange(e){
-        setPass(e.target.value)
-    }
+  function Send() {
+    console.log(id, password);
+    //api 통해서 로그인 보내고 아이디 일치시
+    activate(injected);
+    navigate("/doc");
+  }
 
-    function Send(){
-        console.log(id,password)
-        //api 통해서 로그인 보내고 아이디 일치시
-        activate(injected)
-        navigate('/doc')
-    }
-
-    return(
-        <div id="loginbox">
-            <div class = "login">
-                <h1 class = "title">병원로그인</h1>
-                <p>약쏙(전자처방전)을 이용하기 위해서는 회원가입이 필요합니다</p>
-                <label >아이디</label>
-                <input onChange = {IdChange} class = "a"/>
-                <label>비밀번호</label>
-                <input onChange = {PasswordChange} type="password" class = "a"/>
-                <button onClick={Send} id = "loginbutton">로그인</button>
-                <Link to="/" class = "link" >아이디/비밀번호 찾기</Link>
-            </div>
-            <div class = "info">
-                <img class = "logo"/>
-                <div class = "buttonbox">
-                    <Link to="/doc/signup" class = "linkbutton">회원가입</Link>
-                    <Link to="/" class = "linkbutton">의사가이드</Link>
-                </div>
-            </div>
+  return (
+    <div id="loginbox">
+      <div class="login">
+        <h1 class="title">병원로그인</h1>
+        <p>약쏙(전자처방전)을 이용하기 위해서는 회원가입이 필요합니다</p>
+        <label>아이디</label>
+        <input onChange={IdChange} class="a" />
+        <label>비밀번호</label>
+        <input onChange={PasswordChange} type="password" class="a" />
+        <button onClick={Send} id="loginbutton">
+          로그인
+        </button>
+        <Link to="/" class="link">
+          아이디/비밀번호 찾기
+        </Link>
+      </div>
+      <div class="info">
+        <img class="logo" />
+        <div class="buttonbox">
+          <Link to="/doc/signup" class="linkbutton">
+            회원가입
+          </Link>
+          <Link to="/" class="linkbutton">
+            의사가이드
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
