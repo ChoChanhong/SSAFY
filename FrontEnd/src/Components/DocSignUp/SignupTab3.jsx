ad733bc5e240716b08af583c0be349198cb075d5
김가흔 <rkgms0712@naver.com>
2022-09-28 01:16:56 +0900

    DocSignup

--- FrontEnd/src/Components/DocSignUp/SignupTab3.jsx
+++ FrontEnd/src/Components/DocSignUp/SignupTab3.jsx
@@ -1,15 +1,12 @@
-import {React,useState} from "react"
-import { Link } from 'react-router-dom';
-export default function Signup3(){
-    
-
-
-    return(
-     <div>
-        <div>가입이 완료 되었습니다</div>
-        <div>약쏙을 시작해 보세요</div>
-        <Link to="/doc/login">로그인</Link>
-        <Link to="">의사 가이드</Link>
-     </div>   
-    )
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+export default function Signup3() {
+  return (
+    <div>
+      <div>가입이 완료 되었습니다</div>
+      <div>약쏙을 시작해 보세요</div>
+      <Link to="/doc/login">로그인</Link>
+      <Link to="">의사 가이드</Link>
+    </div>
+  );
 }
--- FrontEnd/src/pages/Doc/Signup.jsx
+++ FrontEnd/src/pages/Doc/Signup.jsx
@@ -1,35 +1,36 @@
-import {React,useState} from "react"
-import DocNavbar from "../../Components/DocNavbar"
-import Signup1 from "../../Components/DocSignUp/SignupTab1"
-import Signup2 from "../../Components/DocSignUp/SignupTab2"
-import Signup3 from "../../Components/DocSignUp/SignupTab3"
-import "./Signup.css"
+import { React, useState } from "react";
+import DocNavbar from "../../Components/DocNavbar";
+import Signup1 from "../../Components/DocSignUp/SignupTab1";
+import Signup2 from "../../Components/DocSignUp/SignupTab2";
+import Signup3 from "../../Components/DocSignUp/SignupTab3";
+import "./Signup.css";
 
-export function Signup(){
+export function Signup() {
+  const [step, setStep] = useState(1);
+  const tab = {
+    1: <Signup1 setStep={setStep} />,
+    2: <Signup2 setStep={setStep} />,
+    3: <Signup3 setStep={setStep} />,
+  };
 
-    const [step,setStep] = useState(1)
-    const tab = { 1:<Signup1 setStep={setStep}/> , 2:<Signup2 setStep={setStep}/> , 3:<Signup3 setStep={setStep}/>}
-
-    return(
-        <div>
-            <DocNavbar id = "Navbar"/>
-            <div id = "signuptabbar">
-                <div class = {step===1 ? "signuptabSelected" : "signuptab"}>
-                    <p>Step 01</p>
-                    <p>이용약관 동의</p>
-                </div>
-                <div class = {step===2 ? "signuptabSelected" : "signuptab"}>
-                    <p>Step 02</p>
-                    <p>회원정보 작성</p>
-                </div>
-                <div class = {step===3 ? "signuptabSelected" : "signuptab"}>
-                    <p>Step 03</p>
-                    <p>회원가입 완료</p>
-                </div>
-            </div>
-            <div>
-                {tab[step]}
-            </div>
+  return (
+    <div>
+      <DocNavbar id="Navbar" />
+      <div id="signuptabbar">
+        <div className={step === 1 ? "signuptabSelected" : "signuptab"}>
+          <span style={{fontSize:"20px"}}>Step 01</span>
+          <span style={{fontSize:"13px"}}>이용약관 동의</span>
         </div>
-    )
+        <div className={step === 2 ? "signuptabSelected" : "signuptab"}>
+          <span style={{fontSize:"20px"}}>Step 02</span>
+          <span style={{fontSize:"13px"}}>회원정보 작성</span>
+        </div>
+        <div className={step === 3 ? "signuptabSelected" : "signuptab"}>
+          <span style={{fontSize:"20px"}}>Step 03</span>
+          <span style={{fontSize:"13px"}}>회원가입 완료</span>
+        </div>
+      </div>
+      <div>{tab[step]}</div>
+    </div>
+  );
 }
