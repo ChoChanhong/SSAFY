ca2fe0e9dcd0fccce71e56758e21a1137d5f525c
김가흔 <rkgms0712@naver.com>
2022-09-28 17:52:56 +0900

    FE/Ph

--- FrontEnd/src/Components/DocMainNav.jsx
+++ FrontEnd/src/Components/DocMainNav.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./DocNavbar.css";
+import IMG from "../assets/images/001.png";
 
 export default function DocMainNav() {
   return (
     <div id="nav">
       <div>
-        <img style={{ height: 100 }} src="img/001.png" alt="로고" />
+        <img id="logoImg" src={IMG} alt="logo" />
       </div>
     </div>
   );
