8767521ec0f8011d3ad70935b7feab5196091050
김가흔 <rkgms0712@naver.com>
2022-09-28 13:48:18 +0900

    FE/DocSignup

--- FrontEnd/src/Components/DocSignUp/SignupTab3.jsx
+++ FrontEnd/src/Components/DocSignUp/SignupTab3.jsx
@@ -1,12 +1,44 @@
+import { borderLeft } from "@mui/system";
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
+import "./DocSignup.css";
+
 export default function Signup3() {
   return (
-    <div>
-      <div>가입이 완료 되었습니다</div>
-      <div>약쏙을 시작해 보세요</div>
-      <Link to="/doc/login">로그인</Link>
-      <Link to="">의사 가이드</Link>
+    <div className="signBox" style={{ textAlign: "center", marginTop: 150 }}>
+      <p className="successTitle">가입이 완료 되었습니다</p>
+      <p className="subTitle">약쏙을 시작해 보세요</p>
+      <div style={{ marginTop: 80 }}>
+        <Link to="/doc/login">
+          <button
+            className="button"
+            style={{
+              borderColor: "transparent",
+              backgroundColor: "#5FD068",
+              width: 400,
+              height: 50,
+              fontSize: 25,
+            }}
+          >
+            로그인
+          </button>
+        </Link>
+      </div>
+      <div style={{ marginTop: 40 }}>
+        <Link to="">
+          <button
+            className="button"
+            style={{
+              borderColor: "transparent",
+              width: 400,
+              height: 50,
+              fontSize: 25,
+            }}
+          >
+            의사 가이드
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
