a9b7a7c437adbb93b9d08c23cbc273fec40d8e02
김가흔 <rkgms0712@naver.com>
2022-10-01 00:04:48 +0900

    FE/DocGen

--- FrontEnd/src/pages/Doc/Generate.jsx
+++ FrontEnd/src/pages/Doc/Generate.jsx
@@ -1,23 +1,21 @@
-import React from "react"
-import {Link} from "react-router-dom"
-import DocNavbar from "../../Components/DocNavbar"
-import CliInfo from "../../Components/Generate/CliInfo"
-import PerInfo from "../../Components/Generate/Perinfo"
-import PerDetail from "../../Components/Generate/PerDetail"
+import React from "react";
+import { Link } from "react-router-dom";
+import DocNavbar from "../../Components/DocNavbar";
+import CliInfo from "../../Components/Generate/CliInfo";
+import PerInfo from "../../Components/Generate/Perinfo";
+import PerDetail from "../../Components/Generate/PerDetail";
 
-
-export default function Generate(){
-    return(
-        <div>
-            <div>
-                <DocNavbar/>
-            </div>
-            <div>
-                <CliInfo/>
-                <PerInfo/>
-                <PerDetail/>
-            </div>
-        </div>
-        
-    )
+export default function Generate() {
+  return (
+    <div>
+      <div>
+        <DocNavbar />
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-evenly", margin: "50px 50px 0px 50px" }}>
+        <CliInfo />
+        <PerInfo />
+        <PerDetail />
+      </div>
+    </div>
+  );
 }
