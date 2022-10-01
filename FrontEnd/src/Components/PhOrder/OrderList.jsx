e821a91837aeaceb5c7f2562e234ddc993e56ee5
김가흔 <rkgms0712@naver.com>
2022-10-01 22:27:52 +0900

    FE/Ph

---
+++ FrontEnd/src/Components/PhOrder/OrderList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrderList() {
+  return (
+    <div>
+      <h1>접수 리스트</h1>
+    </div>
+  )
+}
--- FrontEnd/src/pages/Ph/Order.jsx
+++ FrontEnd/src/pages/Ph/Order.jsx
@@ -1,29 +1,46 @@
 import REACT, { useState } from "react";
 import DaumPostCode from "react-daum-postcode";
+import PerDetail from "../../Components/Generate/PerDetail";
 import PhNavbar from "../../Components/PhNavbar";
+import OrderInfo from "../../Components/PhOrder/OrderInfo";
+import OrderList from "../../Components/PhOrder/OrderList";
+
 export default function Order() {
-  const handleComplete = (data) => {
-    let fullAddress = data.address;
-    let extraAddress = "";
-    if (data.addressType === "R") {
-      if (data.bname !== "") {
-        extraAddress += data.bname;
-      }
-      if (data.buildingName !== "") {
-        extraAddress +=
-          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
-      }
-      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
-    }
-    console.log(fullAddress);
-    //fullAddress -> 전체 주소반환
-  };
+  // const handleComplete = (data) => {
+  //   let fullAddress = data.address;
+  //   let extraAddress = "";
+  //   if (data.addressType === "R") {
+  //     if (data.bname !== "") {
+  //       extraAddress += data.bname;
+  //     }
+  //     if (data.buildingName !== "") {
+  //       extraAddress +=
+  //         extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+  //     }
+  //     fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+  //   }
+  //   console.log(fullAddress);
+  //fullAddress -> 전체 주소반환
+  // };
   return (
     <div>
       {/* <div>dddddd</div>
             <DaumPostCode onComplete={handleComplete} className="post-code" />
             <div>ddddd</div> */}
-      <PhNavbar />
+      <div>
+        <PhNavbar />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          margin: "30px 50px 0px 50px",
+        }}
+      >
+        <OrderList />
+        <PerDetail />
+        <OrderInfo />
+      </div>
     </div>
   );
 }
