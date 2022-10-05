d783c8a19f5be1839b7c54bbc09849c9d5323896
김가흔 <rkgms0712@naver.com>
2022-10-06 02:02:27 +0900

    FE/Doc

--- FrontEnd/src/Components/Doc/Generate/CliInfo.jsx
+++ FrontEnd/src/Components/Doc/Generate/CliInfo.jsx
@@ -1,17 +1,17 @@
 import axios from "axios";
-import setAuthorizationToken from '../../../utils/AuthorizationToken'
+import setAuthorizationToken from "../../../utils/AuthorizationToken";
 import { React, useEffect, useState } from "react";
 import "./Cliinfo.css";
 
 export default function CliInfo(props) {
   const [Snum, setSnum] = useState(""); //환자이름
-  const [name, setName] = useState(''); //환자주소
-  const [email, setEmail] = useState(''); //환자주소
-  const [wallet, setWallet] = useState(''); //환자주소
-  
+  const [name, setName] = useState(""); //환자주소
+  const [email, setEmail] = useState(""); //환자주소
+  const [wallet, setWallet] = useState(""); //환자주소
+
   const URL = "https://j7e205.p.ssafy.io/api/search";
-  const token = localStorage.getItem("login-token")
-  
+  const token = localStorage.getItem("login-token");
+
   function numberCheck(e) {
     const a = e.target.value.replace(/^(\d{6})(\d{7})$/, `$1-$2`);
     setSnum(a);
@@ -20,24 +20,22 @@ export default function CliInfo(props) {
     }
   }
 
-  function search(){
-    console.log(name,Snum)
-    setAuthorizationToken(token)
+  function search() {
+    console.log(name, Snum);
+    setAuthorizationToken(token);
     axios
-    .post(URL,{ patientName : name, patientRRN : Snum })
-    .then(function(res){
-      console.log(res.data)
-      setEmail(res.data.userEmail)
-      setWallet(res.data.userWalletAddress)
-      props.changeUserSeq(res.data.userSeq)
-      props.changeName(name)
-      props.changeWallet(res.data.userWalletAddress)
-
-    })
-    .catch(function(err){
-      alert('환자를 찾을 수 없습니다')
-    }
-  )
+      .post(URL, { patientName: name, patientRRN: Snum })
+      .then(function (res) {
+        console.log(res.data);
+        setEmail(res.data.userEmail);
+        setWallet(res.data.userWalletAddress);
+        props.changeUserSeq(res.data.userSeq);
+        props.changeName(name);
+        props.changeWallet(res.data.userWalletAddress);
+      })
+      .catch(function (err) {
+        alert("환자를 찾을 수 없습니다");
+      });
   }
 
   return (
@@ -48,7 +46,13 @@ export default function CliInfo(props) {
         </div>
         <div id="genBox">
           <label id="genLabel">환자명</label>
-          <input id="genInput" value = {name} onChange={(e)=>{setName(e.target.value)}}/>
+          <input
+            id="genInput"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
         <div id="genBox">
           <label id="genLabel">주민번호</label>
@@ -56,19 +60,16 @@ export default function CliInfo(props) {
         </div>
         <div id="genBox">
           <label id="genLabel">이메일</label>
-          <input style={{ width: 200 }} value={email} readOnly />
+          <input style={{ width: 215 }} value={email} readOnly />
         </div>
         <div id="genBox">
           <label id="genLabel">지갑</label>
-          <input
-            id="genInput"
-            style={{ marginLeft: 41 }}
-            value={wallet}
-            readOnly
-          />
+          <input id="genInput" style={{ width: 215 }} value={wallet} readOnly />
         </div>
-        <div>
-          <button onClick={search}>조회</button>
+        <div style={{textAlign:"center"}}>
+          <button id="findButton" onClick={search}>
+            조회
+          </button>
         </div>
       </div>
       <div>
--- FrontEnd/src/Components/PhLog/PhLog.jsx
+++ FrontEnd/src/Components/PhLog/PhLog.jsx
@@ -5,7 +5,7 @@ export default function PhLog() {
   return (
     <div id="logLine">
       <div id="phlogName">
-        <p style={{ marginTop: 10, marginLeft: 10 }}>처방 정보</p>
+        <p style={{ marginTop: 10, marginLeft: 10 }}>조제 이력</p>
       </div>
     </div>
   );
