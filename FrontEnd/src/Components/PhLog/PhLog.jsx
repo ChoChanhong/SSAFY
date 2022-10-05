6a43c032b89caa3a0e77113b2e00e96e1f6137a2
김가흔 <rkgms0712@naver.com>
2022-10-06 01:29:04 +0900

    update PhLog

--- FrontEnd/src/Components/PhLog/PhLog.jsx
+++ FrontEnd/src/Components/PhLog/PhLog.jsx
@@ -4,7 +4,9 @@ import "./PhLog.css";
 export default function PhLog() {
   return (
     <div id="logLine">
-      <h1>조제 이력 조회</h1>
+      <div id="phlogName">
+        <p style={{ marginTop: 10, marginLeft: 10 }}>처방 정보</p>
+      </div>
     </div>
   );
 }
