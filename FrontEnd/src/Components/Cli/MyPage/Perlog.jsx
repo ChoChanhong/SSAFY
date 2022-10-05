f6aae411a747630bda4240c167459f493f26945c
김가흔 <rkgms0712@naver.com>
2022-10-06 00:32:25 +0900

    Perlog

--- FrontEnd/src/Components/Cli/MyPage/Perlog.jsx
+++ FrontEnd/src/Components/Cli/MyPage/Perlog.jsx
@@ -2,32 +2,44 @@ import Web3 from "web3";
 import React, { useState, useEffect } from "react";
 import { abi, nftCA } from "../../../web3Config";
 
-
-
 export default function Perlog(props) {
-
-  const list = props.list.map((yak)=><div>
-                                        <div>{yak[1]}</div>
-                                        <div>발행일 :{yak.pubDate}</div>
-                                        <div>{yak.prescriptionCount === '1' ? '단기' : '정기' }</div>
-                                        <button>상세내역</button>
-                                      </div>)
+  const list = props.list.map((yak) => (
+    <div
+      style={{
+        borderBottom: "solid 2px #5681EF",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ margin: "10px 0px 10px 25px" }}>
+        <div
+          style={{ fontWeight: "bold", fontSize: "20px", marginBottom: "10px" }}
+        >
+          {yak[1]}
+        </div>
+        <div>발행일 :　{yak.pubDate}</div>
+        <div
+          style={{ margintTop: "15px" }}
+          className={yak.prescriptionCount === "1" ? "short" : "long"}
+        >
+          {yak.prescriptionCount === "1" ? "단기" : "정기"}
+        </div>
+      </div>
+      <button id="bluebutton" style={{ margin: "40px 10px 10px 0px" }}>
+        상세내역
+      </button>
+    </div>
+  ));
 
   useEffect(() => {
-    console.log(props.list,'처방이력페이지')
-    console.log(props.list[0][1])
+    console.log(props.list, "처방이력페이지");
+    console.log(props.list[0][1]);
   }, []);
-
-
-
 
   return (
     <div>
       <div className="myBox">
-        <div style={{ margin: 20 }}>처방 이력</div>
-        <div>
-          {list}
-        </div>
+        <div>{list}</div>
       </div>
     </div>
   );
